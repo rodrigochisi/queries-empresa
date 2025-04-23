d7e1771306b851693b42f7e6e232f999679ec979
rodrigochisi <rodrigo_franc@live.com>
2025-04-22 21:57:02 -0300

    Update INS_CC_MON_ALTA_MED_HOSP.sql

--- variaveis_command_center/grupo_santa/Urgencia/INS_CC_MON_ALTA_MED_HOSP.sql
+++ variaveis_command_center/grupo_santa/Urgencia/INS_CC_MON_ALTA_MED_HOSP.sql
@@ -1,68 +1,70 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2821
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww14240\viewh12300\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+-- Verifica e remove a view se já existir
+BEGIN
+   EXECUTE IMMEDIATE 'DROP VIEW INVISUAL.INS_CC_MON_ALTA_MED_HOSP';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+/
 
-\f0\fs24 \cf0 -- ======================================================================================\
--- VIEW: INS_CC_MON_ALTA_MED_HOSP\
--- PROP\'d3SITO: Monitora alta hospitalar de pacientes com alta m\'e9dica\
--- CRIADO POR: Rodrigo\
--- DATA DE CRIA\'c7\'c3O: 15/04/2025\
--- \'daLTIMA ALTERA\'c7\'c3O: 15/04/2025 por Fulano - [JIRA-123] Ajuste em filtros de data\
--- ======================================================================================\
-\
-\
- CREATE OR REPLACE FORCE EDITIONABLE VIEW "INVISUAL"."INS_CC_MON_ALTA_MED_HOSP"  AS\
- \
- with Tbl_Mon_Alta_Hosp As ( \
-  select distinct \
-  atend.HR_ALTA_MEDICA,\
-  atend.CD_MULTI_EMPRESA CODIGOMULTIEMPRESA,\
-atend.CD_ATENDIMENTO AS CODIGOATENDIMENTO,\
-TO_CHAR(atend.HR_ALTA_MEDICA, 'YYYY-MM-DD HH24:MI:SS') as DATAHORAINICIO,\
-atend.CD_PACIENTE AS CODIGOPACIENTE,\
-pac.NM_PACIENTE AS NOMEPACIENTE,\
-conv.NM_CONVENIO as DESCRICAOCONVENIO,\
-atend.CD_ORI_ATE as CODIGOITEM,\
-ori.DS_ORI_ATE as DESCRICAOITEM,\
-esp.DS_ESPECIALID as DESCRICAOESPECIALIDADE,\
-prest.NM_PRESTADOR as NOMESOLICITANTE,\
-round((SYSDATE - atend.HR_ALTA_MEDICA) * 1440) as VALOR\
-\
-from dbamv.atendime atend \
-left join dbamv.ori_ate ori on ori.CD_ORI_ATE=atend.CD_ORI_ATE\
-left join dbamv.setor setor on setor.CD_SETOR=ori.CD_SETOR\
-left join dbamv.sacr_tempo_processo_historico hist on hist.CD_ATENDIMENTO=atend.CD_ATENDIMENTO\
-left join dbamv.paciente pac on atend.CD_PACIENTE=pac.CD_PACIENTE\
-left join dbamv.prestador prest on prest.CD_PRESTADOR=atend.CD_PRESTADOR\
-left join dbamv.convenio conv on atend.CD_CONVENIO=conv.CD_CONVENIO\
-left join dbamv.con_pla pla on atend.CD_CONVENIO=pla.CD_CONVENIO and atend.CD_CON_PLA=pla.CD_CON_PLA\
-left join dbamv.especialid esp on esp.CD_ESPECIALID=atend.CD_ESPECIALID\
-\
-Where ( (atend.HR_ALTA_MEDICA BETWEEN SYSDATE - INTERVAL '24' HOUR AND SYSDATE + INTERVAL '24' HOUR)\
-        OR (atend.DT_ALTA_MEDICA BETWEEN SYSDATE - INTERVAL '24' HOUR AND SYSDATE + INTERVAL '24' HOUR)\
-       )  \
-and atend.HR_ALTA_MEDICA is not null        \
-and atend.DT_ALTA is null        \
-\
-\
-)\
-\
-select  Mah.CODIGOMULTIEMPRESA,\
-        Mah.CODIGOATENDIMENTO,\
-        Mah.DATAHORAINICIO,\
-        Mah.CODIGOPACIENTE,\
-        Mah.NOMEPACIENTE,\
-        Mah.DESCRICAOCONVENIO,\
-        Mah.CODIGOITEM,\
-        Mah.DESCRICAOITEM,\
-        Mah.DESCRICAOESPECIALIDADE,\
-        Mah.NOMESOLICITANTE,\
-        Mah.VALOR\
- From Tbl_Mon_Alta_Hosp Mah        \
-  where Mah.HR_ALTA_MEDICA>=(sysdate - interval '24' hour)\
-  order by Mah.HR_ALTA_MEDICA;\
-\
-}
+-- ======================================================================================
+-- VIEW: INS_CC_MON_ALTA_MED_HOSP
+-- VARIÁVEL COMMAND CENTER : Leitos - Tempo de Alta Hospitalar 
+-- PROPÓSITO: Monitora alta hospitalar de pacientes com alta médica
+-- CRIADO POR: Rodrigo
+-- DATA DE CRIAÇÃO:  ** 2024
+-- ÚLTIMA ALTERAÇÃO: 15/04/2025 por Rodrigo - [JIRA-CC-241] Orientação sobre a criação de 2 novas regras , aproveitando essa variável
+-- ======================================================================================
+
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "INVISUAL"."INS_CC_MON_ALTA_MED_HOSP" AS
+
+WITH Tbl_Mon_Alta_Hosp AS (
+  SELECT DISTINCT
+    atend.HR_ALTA_MEDICA,
+    atend.CD_MULTI_EMPRESA AS CODIGOMULTIEMPRESA,
+    atend.CD_ATENDIMENTO AS CODIGOATENDIMENTO,
+    TO_CHAR(atend.HR_ALTA_MEDICA, 'YYYY-MM-DD HH24:MI:SS') AS DATAHORAINICIO,
+    atend.CD_PACIENTE AS CODIGOPACIENTE,
+    pac.NM_PACIENTE AS NOMEPACIENTE,
+    conv.NM_CONVENIO AS DESCRICAOCONVENIO,
+    atend.CD_ORI_ATE AS CODIGOITEM,
+    ori.DS_ORI_ATE AS DESCRICAOITEM,
+    esp.DS_ESPECIALID AS DESCRICAOESPECIALIDADE,
+    prest.NM_PRESTADOR AS NOMESOLICITANTE,
+    ROUND((SYSDATE - atend.HR_ALTA_MEDICA) * 1440) AS VALOR
+
+  FROM DBAMV.ATENDIME atend
+  LEFT JOIN DBAMV.ORI_ATE ori ON ori.CD_ORI_ATE = atend.CD_ORI_ATE
+  LEFT JOIN DBAMV.SETOR setor ON setor.CD_SETOR = ori.CD_SETOR
+  LEFT JOIN DBAMV.SACR_TEMPO_PROCESSO_HISTORICO hist ON hist.CD_ATENDIMENTO = atend.CD_ATENDIMENTO
+  LEFT JOIN DBAMV.PACIENTE pac ON atend.CD_PACIENTE = pac.CD_PACIENTE
+  LEFT JOIN DBAMV.PRESTADOR prest ON prest.CD_PRESTADOR = atend.CD_PRESTADOR
+  LEFT JOIN DBAMV.CONVENIO conv ON atend.CD_CONVENIO = conv.CD_CONVENIO
+  LEFT JOIN DBAMV.CON_PLA pla ON atend.CD_CONVENIO = pla.CD_CONVENIO AND atend.CD_CON_PLA = pla.CD_CON_PLA
+  LEFT JOIN DBAMV.ESPECIALID esp ON esp.CD_ESPECIALID = atend.CD_ESPECIALID
+
+  WHERE (
+          (atend.HR_ALTA_MEDICA BETWEEN SYSDATE - INTERVAL '24' HOUR AND SYSDATE + INTERVAL '24' HOUR)
+       OR (atend.DT_ALTA_MEDICA BETWEEN SYSDATE - INTERVAL '24' HOUR AND SYSDATE + INTERVAL '24' HOUR)
+        )
+    AND atend.HR_ALTA_MEDICA IS NOT NULL
+    AND atend.DT_ALTA IS NULL
+)
+
+SELECT
+  Mah.CODIGOMULTIEMPRESA,
+  Mah.CODIGOATENDIMENTO,
+  Mah.DATAHORAINICIO,
+  Mah.CODIGOPACIENTE,
+  Mah.NOMEPACIENTE,
+  Mah.DESCRICAOCONVENIO,
+  Mah.CODIGOITEM,
+  Mah.DESCRICAOITEM,
+  Mah.DESCRICAOESPECIALIDADE,
+  Mah.NOMESOLICITANTE,
+  Mah.VALOR
+FROM Tbl_Mon_Alta_Hosp Mah
+WHERE Mah.HR_ALTA_MEDICA >= (SYSDATE - INTERVAL '24' HOUR)
+ORDER BY Mah.HR_ALTA_MEDICA;
